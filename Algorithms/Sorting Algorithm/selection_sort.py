9483751b4f0f1de7c5ce24e230e858adf18530d4
MD SANAULLAH <202426422+parz1val37@users.noreply.github.com>
2025-12-24 22:46:00 +0530

    Fix selection sort loop and formatting issues

--- Algorithms/Sorting Algorithm/selection_sort.py
+++ Algorithms/Sorting Algorithm/selection_sort.py
@@ -1,8 +1,15 @@
+''' Time complexity: O(n²)
+    Space complexity: O(1)'''
+
+# Not Adaptive
+# Not Stable
+
 def selection_sort(arr):
-  for i in range(len(arr)):
+  for i in range(len(arr)-1):
     min = i
+
     for j in range(i+1, len(arr)):
-      if arr[j]<arr[min]:
+      if arr[j] < arr[min]:
         min = j
-    arr[i], arr[min] = arr[min], arr[i]
+    arr[i],arr[min] = arr[min],arr[i]
   return arr
